9a327bb7010a62d4a895a54d4cda2fef71e699e6
Jonathan Protzenko <jonathan.protzenko@gmail.com>
2020-01-01 18:21:52 -0500

    More WIP

--- options.js
+++ options.js
@@ -1,14 +1,47 @@
+/* Tab navigation */
+function initTabs() {
+  for (let l of document.querySelectorAll(".tablabel")) {
+    let panel = l.dataset.panel;
+    l.addEventListener("click", function (event) {
+      document.querySelector(".tabpanel.active").classList.remove("active");
+      document.getElementById(panel).classList.add("active");
+    });
+  }
+}
+
+/* Higher-order helpers */
+function moveUp(parent) {
+  return (event) => {
+    let li = parent.querySelector(".selected");
+    if (li && li != li.parentNode.firstElementChild)
+      li.parentNode.insertBefore(li, li.previousElementSibling);
+  }
+}
+
+function moveDown(parent) {
+  return (event) => {
+    let li = parent.querySelector(".selected");
+    if (li && li != li.parentNode.lastElementChild)
+      li.parentNode.insertBefore(li, li.nextElementSibling.nextElementSibling);
+  }
+}
+
+function select(parent) {
+  return (event) => {
+    let s = parent.querySelector(".selected");
+    if (s)
+      s.classList.remove("selected");
+    event.target.classList.add("selected");
+  }
+}
+
 /* Account pane */
 function mkAccount(a) {
   let li = document.createElement("li");
   li.classList.add("account-entry");
-  li.textContent = a.name + " (" +a.type+")";
+  li.textContent = a.name;
   li.dataset.id = a.id;
-  li.addEventListener("click", function (event) {
-    for (let i of document.querySelectorAll(".account-entry"))
-      i.classList.remove("selected");
-    li.classList.add("selected");
-  });
+  li.addEventListener("click", select(document.querySelector("#account-list")));
   return li;
 }
 
@@ -27,15 +60,15 @@ async function applyAccountChanges() {
     let accounts = [];
     for (let li of document.querySelectorAll(".account-entry"))
       accounts.push(li.dataset.id);
-    browser.accounts.setCustomSort(accounts);
+    await browser.accounts.setCustomSort(accounts);
   } else {
-    browser.accounts.clearCustomSort();
+    await browser.accounts.clearCustomSort();
   }
 }
 
 async function initAccountPane() {
   /* Set UI in accordance with what's in the prefs*/
-  let userSort = browser.accounts.hasCustomSort();
+  let userSort = await browser.accounts.hasCustomSort();
   document.querySelector("#custom-sort").checked = userSort;
   showHideAccount();
 
@@ -46,28 +79,78 @@ async function initAccountPane() {
   }
 
   /* Event handlers */
-  document.querySelector("#account-move-up").addEventListener("click", function (event) {
-    let li = document.querySelector(".account-entry.selected");
-    if (li != li.parentNode.firstElementChild)
-      li.parentNode.insertBefore(li, li.previousElementSibling);
-  });
-
-  document.querySelector("#account-move-down").addEventListener("click", function (event) {
-    let li = document.querySelector(".account-entry.selected");
-    if (li != li.parentNode.lastElementChild)
-      li.parentNode.insertBefore(li, li.nextElementSibling.nextElementSibling);
-  });
-
+  document.querySelector("#account-move-up").addEventListener("click",
+    moveUp(document.querySelector("#account-list")));
+  document.querySelector("#account-move-down").addEventListener("click",
+    moveDown(document.querySelector("#account-list")));
   document.querySelector("#custom-sort").addEventListener("change", showHideAccount);
-
   document.querySelector("#account-apply").addEventListener("click", applyAccountChanges);
+}
+
+/* Folder pane */
+
+var gCurrentAccount;
+
+function clearFolders() {
+  let ul = document.querySelector("#folder-list");
+  while (ul.firstChild)
+    ul.removeChild(ul.firstChild);
+}
+
+function fillFolders(ul, folders) {
+  for (let f of folders) {
+    let li = document.createElement("li");
+    li.textContent = f.name;
+    li.classList.add("folder-entry");
+    li.addEventListener("click", select(document.querySelector("#folder-list")));
+
+    if (f.subFolders.length > 0) {
+      let ul = document.createElement("ul");
+      fillFolders(ul, f.subFolders);
+      li.appendChild(ul);
+    }
+
+    ul.appendChild(li);
+  }
+}
+
+async function refreshFolders() {
+  clearFolders();
+
+  let id = document.getElementById("folders-account").value;
+  let a = await browser.accounts.get(id);
+
+  let ul = document.querySelector("#folder-list");
+  fillFolders(ul, a.folders);
+}
+
+function mkOption(a) {
+  let o = document.createElement("option");
+  o.textContent = a.name;
+  o.value = a.id;
+  return o;
+}
+
+async function initFolderPane() {
+  let accounts = await browser.accounts.list();
+  let accountSelect = document.getElementById("folders-account");
+  for (let a of accounts) {
+    accountSelect.appendChild(mkOption(a));
+  }
+  accountSelect.addEventListener("change", refreshFolders);
+  await refreshFolders();
+
+  document.querySelector("#folder-move-up").addEventListener("click",
+    moveUp(document.querySelector("#folder-list")));
+  document.querySelector("#folder-move-down").addEventListener("click",
+    moveDown(document.querySelector("#folder-list")));
 }
 
 /* Global initialization */
 async function initOptions() {
+  initTabs();
   await initAccountPane();
+  await initFolderPane();
 }
 
 document.addEventListener("DOMContentLoaded", initOptions);
-
-console.log("done");
