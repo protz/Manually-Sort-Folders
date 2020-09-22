792b4d6597dc5ca7bcc9b484ad77df1d0945d3fc
itagagaki <itagagaki@gmail.com>
2020-09-22 22:00:07 +0900

    Changed .xul to .xhtml
    Changed .xul to .xhtml because <script ..>..</script> in .xul didn't work on Thunderbird 78. Then it works.

--- content/scripts/messenger.js
+++ content/scripts/messenger.js
@@ -5,11 +5,10 @@ var { Services } = ChromeUtils.import("resource://gre/modules/Services.jsm");
 Services.scriptloader.loadSubScript("chrome://tbsortfolders/content/folderPane.js", window, "UTF-8");
 
 function onLoad(activatedWhileWindowOpen) {
-  WL.injectCSS("resource://quicktext/skin/quicktext.css");
   WL.injectElements(`
     <menupopup id="taskPopup">
       <menuitem insertafter="activityManager" id="tbsf_menu_item"
-        oncommand="window.openDialog('chrome://tbsortfolders/content/', '',
+        oncommand="window.openDialog('chrome://tbsortfolders/content/tbsortfolders.xhtml', '',
           'chrome,titlebar,toolbar,centerscreen');"
         label="&tbsf.menuentry.label;" />
     </menupopup>`,
