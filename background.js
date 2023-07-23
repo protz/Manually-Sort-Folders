70f9b645d26ddd549390e5662c553266720b1ad4
John Bieling <john.bieling@gmx.de>
2023-07-23 14:17:04 +0200

    add html options page

--- background.js
+++ background.js
@@ -1,27 +1,5 @@
 browser.WindowListener.registerDefaultPrefs("defaults/preferences/prefs.js");
 
-browser.WindowListener.registerChromeUrl([ 
-  ["content",  "tbsortfolders",          "content/"],
-  ["resource", "tbsortfolders",          "modules/"],
-  ["locale",   "tbsortfolders", "da",    "locale/da/"],
-  ["locale",   "tbsortfolders", "de",    "locale/de/"],
-  ["locale",   "tbsortfolders", "en-US", "locale/en-US/"],
-  ["locale",   "tbsortfolders", "es-ES", "locale/es-ES/"],
-  ["locale",   "tbsortfolders", "fr",    "locale/fr/"],
-  ["locale",   "tbsortfolders", "it",    "locale/it/"],
-  ["locale",   "tbsortfolders", "ja",    "locale/ja/"],
-  ["locale",   "tbsortfolders", "nl",    "locale/nl/"],
-  ["locale",   "tbsortfolders", "nb-NO", "locale/nb-NO/"],
-  ["locale",   "tbsortfolders", "pl",    "locale/pl/"],
-  ["locale",   "tbsortfolders", "pt",    "locale/pt/"],
-  ["locale",   "tbsortfolders", "pt-BR", "locale/pt-BR/"],
-  ["locale",   "tbsortfolders", "ru-RU", "locale/ru-RU/"],
-  ["locale",   "tbsortfolders", "sk",    "locale/sk/"],
-  ["locale",   "tbsortfolders", "sr",    "locale/sr/"],
-  ["locale",   "tbsortfolders", "sv-SE", "locale/sv-SE/"],
-  ["locale",   "tbsortfolders", "zh-CN", "locale/zh-CN/"],
-  ["locale",   "tbsortfolders", "zh-TW", "locale/zh-TW/"],
-]);
 
 // For Thunderbird 78.0 and later
 browser.WindowListener.registerWindow(
